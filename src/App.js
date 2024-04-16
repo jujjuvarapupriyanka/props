b6043bf66c4ea8b0d9f95952fe666ece9eb6972c
jujjuvarapupriyanka <jujjuvarapu.priyanka051@gmail.com>
2024-04-16 03:11:44 -0700

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ListImages from './components/prop/props';
+import Listitems from './components/prop/propimages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListImages></ListImages>
+      <Listitems></Listitems>
+
     </div>
   );
 }
---
+++ src/components/prop/propimages.js
@@ -0,0 +1,13 @@
+
+
+const Listitems =({hello})=>{
+    console.log(hello)
+    return(
+        <>
+            <img src={hello.imageUrl} alt="display the image"/>
+        </>
+        
+    )
+
+}
+export default Listitems;
